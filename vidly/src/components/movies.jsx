4bbb77c4b266b46bf63f101cc94dd0c0201fd29e
Neel Bavarva <neelbavarva5@gmail.com>
2020-08-31 11:46:35 +0530

    font aw

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {getMovies} from '../services/fakeMovieService';
+import Like from './common/like';
 
 class Movies extends Component {
     state = { 
@@ -11,11 +12,21 @@ class Movies extends Component {
         this.setState({movies});
     }
 
+    handleLike = movie => {
+        const movies = [...this.state.movies];
+        const index = movies.indexOf(movie);
+        movies[index]={...movies[index]};
+        movies[index].liked=!movies[index].liked;
+        this.setState({movies});
+    }
+
+    
+
     render() { 
         const {length: count} = this.state.movies;
 
         if(count===0 ) 
-            return <p>There are no movies in the database</p>
+            return <h6 className="m-3">There are no movies in the database</h6>
         return ( 
             <React.Fragment>
             <h6 className="m-3">Showing {count} movies in the database</h6>
@@ -26,16 +37,20 @@ class Movies extends Component {
                         <th>Genre</th>
                         <th>Stock</th>
                         <th>Rate</th>
-                        <th></th>
+                        <th>Like</th> 
+                       
+                        <th/>
                     </tr>
                 </thead>
                 <tbody>
                     {this.state.movies.map(movie =>
                     <tr key={movie._id}>
                         <td>{movie.title}</td>
+                        
                         <td>{movie.genre.name}</td>
                         <td>{movie.numberInStock}</td>
                         <td>{movie.dailyRentalRate}</td>
+                        <td><Like liked={movie.liked} onClick={() => this.handleLike(movie)}/></td>
                         <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
                     </tr>
                     )}
