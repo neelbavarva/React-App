d6b0c1a5dfb9c723ccbc7bb4ab9daf4292e60887
Neel Bavarva <neelbavarva5@gmail.com>
2020-09-03 05:58:41 +0530

    last commit

--- react-app/src/components/navbar.jsx
+++ react-app/src/components/navbar.jsx
@@ -5,6 +5,7 @@ const Navbar = ({totalCounters}) => {
         return (
             <nav className="navbar navbar-light bg-light">
             <a className="navbar-brand" href="#">Navbar
+            
             <span className="badge badge-pill badge-warning offset-2" >{totalCounters}</span>
             </a>
             </nav>
