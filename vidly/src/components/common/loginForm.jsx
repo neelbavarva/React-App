7b4edd846ff9a01e1cd15eb879143b0307adc8eb
Neel Bavarva <neelbavarva5@gmail.com>
2020-10-26 09:35:19 +0530

    login form added till common errors

--- vidly/src/components/common/loginForm.jsx
+++ vidly/src/components/common/loginForm.jsx
@@ -1,45 +1,47 @@
 import React, { Component } from 'react';
-import Input from "./input";
 
 class LoginForm extends Component {
 
     state = {
-        account : {username: '' , password: ''}
+        account : {username : '' , password : ''}
     }
-
-    username =React.createRef();
-
-    handleChange = e => {
-        const account = {...this.state.account};
-        account[e.currentTarget.name] = e.currentTarget.value;
-        this.setState({account});
-    }
-
+    
     handleSubmit = e => {
         e.preventDefault();
-        const username =this.username.current.value;
-        console.log("submited");
+        
+        console.log("Submited");
+    }
+
+    handleChange = ({currentTarget: input}) => {
+        const account = {...this.state.account};
+        account[input.name] = input.value;
+        this.setState({account});
     }
     
     render() { 
+        const{account} = this.state;
         return ( 
             <div>
-                <br/><br/>
-                <Input 
-                    name="username"
-                    value={this.state.account.username}
-                    label="Username"
-                    onChange={this.handleChange}
-                />
-                <Input 
-                    name="password"
-                    value={this.state.account.password}
-                    label="Password"
-                    onChange={this.handleChange}
-                />
-                <br/>
-                <button onSubmit={this.handleSubmit} className="btn btn-primary">Submit</button>
-                
+                <h1>Login</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="username">Username</label>
+                        <input value={account.username}
+                               onChange={this.handleChange} 
+                               id="username" type="text" 
+                               name="username"
+                               className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input value={account.password}
+                               onChange={this.handleChange}
+                               name="password"
+                               id="password" type="text" 
+                               className="form-control" />
+                    </div>
+                    <button className="btn btn-primary">Login</button>
+                </form>
             </div>
           );
     }
